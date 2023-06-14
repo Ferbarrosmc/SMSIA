f403f36b882c8b090c3421fe87d30be51e8cb1f6
Ferbarrosmc <136300052+Ferbarrosmc@users.noreply.github.com>
2023-06-13 22:48:45 -0400

    Update postmonger.js

--- js/postmonger.js
+++ js/postmonger.js
@@ -1,62 +1,36 @@
-(function () {
+define(['postmonger'], function (Postmonger) {
   'use strict';
 
-  // Importar el objeto Postmonger
-  var Postmonger = window.Postmonger;
-
-  // Crea una instancia de Postmonger
   var connection = new Postmonger.Session();
 
-  // Evento de inicialización
-  connection.on('initActivity', function (data) {
-    // Realiza cualquier configuración inicial que necesites aquí
-    console.log('Actividad inicializada:', data);
-    // Guarda los datos de configuración en caso de que los necesites más adelante
-    // Ejemplo: var activityData = data;
+  $(window).ready(function () {
     connection.trigger('ready');
+
+    connection.on('initActivity', function (data) {
+      // Lógica de inicialización de la actividad
+      // Aquí puedes realizar cualquier configuración necesaria al iniciar la actividad
+      // Puedes acceder a los datos del flujo de trabajo a través del objeto "data"
+    });
+
+    connection.on('requestedTokens', function (tokens) {
+      // Lógica para manejar los tokens solicitados
+      // Aquí puedes acceder a los tokens solicitados (como el token de autenticación JWT)
+      // Los tokens se pasan como parámetro en el objeto "tokens"
+    });
+
+    connection.on('requestedEndpoints', function (endpoints) {
+      // Lógica para manejar los endpoints solicitados
+      // Aquí puedes acceder a los endpoints solicitados (como la URL de la actividad personalizada)
+      // Los endpoints se pasan como parámetro en el objeto "endpoints"
+    });
+
+    connection.on('clickedNext', function () {
+      // Lógica para manejar el clic en el botón "Siguiente" en la interfaz de configuración de la actividad
+      // Aquí puedes realizar cualquier validación o acción adicional antes de pasar al siguiente paso del flujo de trabajo
+      // Si no hay ningún error, debes llamar a la función "connection.trigger('nextStep')" para avanzar al siguiente paso
+    });
+
+    // Otros eventos y lógica necesarios para la interacción con Journey Builder
+
   });
-
-  // Evento de cambio de configuración
-  connection.on('requestedTokens', function (tokens) {
-    // Realiza cualquier acción necesaria cuando se reciben los tokens
-    console.log('Tokens recibidos:', tokens);
-  });
-
-  // Evento de guardado de configuración
-  connection.on('save', function (data) {
-    // Realiza cualquier acción necesaria para guardar la configuración
-    console.log('Datos guardados:', data);
-    connection.trigger('saveActivity', data);
-  });
-
-  // Evento de validación de configuración
-  connection.on('validate', function (data) {
-    // Realiza cualquier validación necesaria en la configuración
-    console.log('Datos validados:', data);
-    connection.trigger('doneValidation', data);
-  });
-
-  // Evento de publicación de la actividad
-  connection.on('publish', function (data) {
-    // Realiza cualquier acción necesaria al publicar la actividad
-    console.log('Actividad publicada:', data);
-    connection.trigger('done');
-  });
-
-  // Evento de cancelación de la actividad
-  connection.on('cancel', function () {
-    // Realiza cualquier acción necesaria al cancelar la actividad
-    console.log('Actividad cancelada');
-    connection.trigger('done');
-  });
-
-  // Conectarse a Journey Builder
-  connection.on('ready', function () {
-    // Indicar que la actividad está lista
-    connection.trigger('ready');
-  });
-
-  // Iniciar la conexión con Journey Builder
-  connection.init();
-
-})();
+});
